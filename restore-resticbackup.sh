d8560128b828a761c2d68c2f463c433ab9415129
Laxman Singh <jodhpur.laxman@gmail.com>
2024-04-12 18:20:22 +0530

    Update restore-resticbackup.sh

--- restore-resticbackup.sh
+++ restore-resticbackup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "which domain backup you want to restore"
 read username
-AWS_ACCESS_KEY_ID=$(ls /home/cyberpanel/aws/); AWS_SECRET_ACCESS_KEY=$(cat /home/cyberpanel/aws/$AWS_ACCESS_KEY_ID); restic -r s3:usc1.contabostorage.com/$username snapshot
+AWS_ACCESS_KEY_ID=$(ls /home/cyberpanel/aws/); AWS_SECRET_ACCESS_KEY=$(cat /home/cyberpanel/aws/$AWS_ACCESS_KEY_ID); restic -r s3:usc1.contabostorage.com/$username snapshots 
 echo "which snapshot you want to restore"
 read snapshotid
 AWS_ACCESS_KEY_ID=$(ls /home/cyberpanel/aws/); AWS_SECRET_ACCESS_KEY=$(cat /home/cyberpanel/aws/$AWS_ACCESS_KEY_ID); restic -r s3:usc1.contabostorage.com/$username restore $snapshotid --target /backup/ >> /home/cyberpanel/incbackup-cs.txt
