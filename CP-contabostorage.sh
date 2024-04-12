d8b54bbdef9c95612c1cf3d44218f310d3a24c10
Laxman Singh <jodhpur.laxman@gmail.com>
2024-04-12 15:57:55 +0530

    Update CP-contabostorage.sh

--- CP-contabostorage.sh
+++ CP-contabostorage.sh
@@ -1,2 +1,3 @@
 sed -i 's/s3.amazonaws.com/usc1.contabostorage.com/g' /usr/local/CyberCP/IncBackups/IncBackupsControl.py
+sed -i 's/s3.amazonaws.com/usc1.contabostorage.com/g' /usr/local/CyberCP/IncBackups/views.py
 service lscpd restart
