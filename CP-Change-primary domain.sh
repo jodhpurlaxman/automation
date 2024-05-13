ff5355349341af3ec40bf89fafa45c3aa1bdc268
Laxman Singh <jodhpur.laxman@gmail.com>
2024-05-13 13:50:37 +0530

    Update CP-Change-primary domain.sh

--- CP-Change-primary domain.sh
+++ CP-Change-primary domain.sh
@@ -13,21 +13,22 @@ else
     
     #backup the existing file
     cp ${filename} ${filename}.LX
-    
     #Add Public in existing path 
     sed -i 's#public_html#'public_html/public'#g' ${filename}
     #sed -i 's#cgi-bin#'public/cgi-bin'#g' ${filename}
-    
     #delete Cache file
     rm -vf ${filename}.cache
 
     #Backup SSL file
     cp ${filename}_SSL ${filename}_SSL.LX
+    #Update Path in SSL file
     sed -i 's#public_html#'public_html/public'#g' ${filename}_SSL
-    
     #delete Cache file
     rm -vf ${filename}_SSL.cache
+    
     /scripts/updateuserdatacache && /scripts/rebuildhttpdconf && /scripts/restartsrv_httpd
+    
+    
     #backup the existing file
     cp ${filename}.php-fpm.yaml ${filename}.php-fpm.yaml.LX
     echo "php_admin_value_doc_root: { name: 'php_admin_value[doc_root]', value: /home/${cpanel_username}/public_html/public }" >> $filename.php-fpm.yaml
